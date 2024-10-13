4971dcd671eed35679b204a2d69568e31d86ffa8
Jantenorcruzz <Jantenorcruzz@gmail.com>
2024-10-13 14:08:24 +0800

    api integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,63 @@
 import { useState } from "react";
 import Weather from "./components/Weather";
-
+import axios from "axios";
 function App() {
+  const [cityName, setCityName] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=Metric&appid=af9dd1d991b49e1a548f3417b40b16c4`;
+
+  const [apiData, setApiData] = useState(null);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setCityName(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (cityName === null || cityName === "") {
+      setApiData(null);
+      return;
+    }
+
+    try {
+      const res = await axios.get(url);
+
+      if (!res || !res.data) {
+        setApiData(undefined);
+        return;
+      }
+      setApiData(res.data);
+    } catch (error) {
+      setApiData(undefined);
+    }
+  };
+
+  let searchContent = <p className="text-2xl font-bold text-neutral-600 text-center">Search city name</p>
+
+  if(apiData === undefined){
+    searchContent = <p className="text-2xl font-bold text-neutral-600 text-center">City not found!</p>
+  }else if(apiData){
+    searchContent = <Weather data={apiData} />
+  }else{
+    searchContent = <p  className="text-2xl font-bold text-neutral-600 text-center">Search city name</p>
+  }
+
+  
   return (
     <div className="h-dvh flex justify-center items-center text-neautral-600">
       <div className="shadow-2xl p-20 roubded-md flex flex-col gap-4">
         <h1 className="text-4xl text-center mb-8">Weather App</h1>
-        <form>
-          <input className="bg-transparent border-2 rounded-2xl w-full px-4 text-center" placeholder="Enter City Name" type="search" />
+        <form onSubmit={handleSubmit}>
+          <input
+            className="bg-transparent border-2 rounded-2xl w-full px-4 text-center"
+            placeholder="Enter City Name"
+            type="search"
+            onChange={handleChange}
+          />
         </form>
-        <Weather />
+        {searchContent}
       </div>
     </div>
   );
--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import sampleImg from '../assets/react.svg'
-function Weather() {
+import _ from 'lodash'
+function Weather({data}) {
   return (
     <div className='flex flex-col gap-4'>
-        <h1 className='text-4xl font-bold text-center'>Quezon City</h1>
+        <h1 className='text-4xl font-bold text-center'>{data.name}</h1>
         <img src={sampleImg} className='h-40' />
-        <p className='text-center text-2xl font-bold'>Sunny</p>
+        <p className='text-center text-2xl font-bold'>{_.capitalize(data.weather[0].description)}</p>
         <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-4xl'>27°C</h1>
+            <h1 className='text-4xl'>{data.main.temp}°C</h1>
             <h4 className='text-lg font-medium'>Temperature</h4>
         </div>
         <div className='grid grid-cols-2 gap-4'>
             <div className='flex flex-col justify-center items-center p-8 bg-slate-50/20 rounded-xl'>
                 <h4 className='text-lg font-medium'>Humidity</h4>
-                <p className='text-lg'>20%</p>
+                <p className='text-lg'>{data.main.humidity}%</p>
             </div>
             <div className='flex flex-col justify-center items-center p-8 bg-slate-50/20 rounded-xl'>
                 <h4 className='text-lg font-medium'>Wind</h4>
-                <p className='text-lg'>5 m/s</p>
+                <p className='text-lg'>{data.wind.speed} m/s</p>
             </div>
         </div>
     </div>
